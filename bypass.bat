ab6951a41544b603ec35c8fbe7e7bce2c19789ad
Kleo08s <kleandrodrizari@gmail.com>
2025-12-04 20:10:42 +0100

    Rename script and update download source

--- bypass.bat
+++ bypass.bat
@@ -1,15 +1,16 @@
 @echo off
 
 echo.
-echo ===== Kleo08s/W11-LocalOOBE =====
+echo ===== Kleo08s/Win11-BypassMSA =====
 echo A script that bypasses Windows requirements and Microsoft account login in OOBE
 echo.
 timeout 5
 echo.
 echo Downloading unattend...
 echo.
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/Kleo08s/W11-LocalOOBE/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/Kleo08s/Win11-BypassMSA/refs/heads/main/unattend.xml
 echo.
 echo Restarting OOBE...
 echo.
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
+
