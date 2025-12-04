f7d80eeeb3c388555bed0bf66c017b9cb9ef32bf
Kleo08s <kleandrodrizari@gmail.com>
2025-12-04 19:52:47 +0100

    Update download link for unattend.xml in bypass.bat

--- bypass.bat
+++ bypass.bat
@@ -8,7 +8,7 @@ pause 10
 echo.
 echo Downloading unattend...
 echo.
-curl -L -o C:\Windows\Panther\unattend.xml https://cdn.gocciola.xyz/notautounattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/Kleo08s/W11-LocalOOBE/refs/heads/main/unattend.xml
 echo.
 echo Restarting OOBE...
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
