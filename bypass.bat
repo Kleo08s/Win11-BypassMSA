45ce526824baaa6909fc906b9cb064fc54632413
Kleo08s <kleandrodrizari@gmail.com>
2025-12-04 19:44:28 +0100

    Add files via upload

---
+++ bypass.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Kleo08s/W11-LocalOOBE
+echo A script that bypasses Windows requirements and Microsoft account login in OOBE
+echo.
+pause 10
+echo.
+echo Downloading unattend...
+echo.
+curl -L -o C:\Windows\Panther\unattend.xml https://cdn.gocciola.xyz/notautounattend.xml
+echo,
+echo Restarting OOBE...
+%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
